330d2bf26b65281d979817fca9ef4e420350dda2
Felix Brandstetter <felix.brandstetter@gmx.de>
2023-05-01 19:54:36 +0200

    Is PDF digital

--- src/main/main.py
+++ src/main/main.py
@@ -1,17 +1,24 @@
-import shutil
-import os
+from wand.image import Image
 import preparation
+import wand
 
 
 def main(file):
     file_format = preparation.get_file_format(file)
     if file_format == "PDF":
         print("Is PDF")
-        #preparation.
+        if preparation.is_pdf_digital(file):
+            print("PDF is digital")
+        else:
+            print("Pdf is scanned")
+    else:
+        print("Is no PDF")
 
-    #preparation.copy_file_to_folder(file,"/RIDSS2023/outputfolder",override=True)
 
 
+
+
+    #preparation.move_file_to_folder(file,"/RIDSS2023/outputfolder",override=True)
 
 if __name__ == "__main__":
     main("/RIDSS2023/inputfolder/Testnotenauszug.pdf")
--- src/main/preparation.py
+++ src/main/preparation.py
@@ -1,15 +1,21 @@
 from wand.image import Image
+import wand
 import os
 import shutil
+import fitz
 
 
 def get_file_format(file):
-    #TODO Failed with .txt Datei
-    with Image(filename=file) as image:
-        return image.format
+    try:
+        with Image(filename=file) as image:
+            return image.format
+    except wand.exceptions.CorruptImageError:
+        # TODO Was passiert mit Bildern die nicht eingelesen werden können
+        print("Bild konnte nicht eingelsen werden")
+        move_file_to_folder(file, "/RIDSS2023/outputfolder/failed", override=True)
 
 
-def copy_file_to_folder(file, destination_folder, override=True):
+def move_file_to_folder(file, destination_folder, override=True):
     if not os.path.exists(destination_folder):
         os.mkdir(destination_folder)
     destination_filepath = os.path.join(destination_folder, os.path.basename(file))
@@ -19,6 +25,20 @@ def copy_file_to_folder(file, destination_folder, override=True):
         if not os.path.exists(destination_filepath):
             shutil.move(file, destination_filepath)
 
-def is_pdf_digital(pdf):
-    #TODO
-    print("ToDo")
+
+def is_pdf_digital(file):
+    pdf_is_digital = True
+    with fitz.open(file) as pdf:
+        for page in pdf:
+            page_text = page.get_text()
+            if len(page_text) < 180:
+                print("Pdf is scanned")
+                pdf_is_digital = False
+                break
+    return pdf_is_digital
+
+def get_metadata(file):
+    metadata = {}
+    with Image(filename=file) as image:
+        metadata.update((k, v) for k, v in image.metadata.items())
+    print(metadata)
