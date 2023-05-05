6766d897fbaf2d2ab6c4b2a23d624fef8270f8ee
uegam <uegam@student.kit.edu>
2023-05-05 17:15:26 +0000

    Eigene Entwicklung

--- src/Eigenentwicklung/main copy.py
+++ src/Eigenentwicklung/main copy.py
@@ -34,7 +34,6 @@ def main(file):
                 timeout=0,
                 pandas_config=None,
             )
-            print(ocr_data)
             for num, box in enumerate(ocr_data.splitlines()):
                 if num != 0:
                     box = box.split()
@@ -44,7 +43,9 @@ def main(file):
                         top = int(box[7])
                         bottom = int(box[7]) + int(box[9])
                         text = box[11]
-                        word = (left, top, right, bottom, text, scale)
+                        height = int(box[9])
+                        width  = int(box[8])
+                        word = (left, top, right, bottom, text, scale, width, height)
                         page = write_word_in_pdf_page(page, word)
 
         pdf.save(file.replace(".pdf", ".ocr.pdf"))
@@ -55,49 +56,13 @@ def write_word_in_pdf_page(pdf_page:Page,word):
     rect = fitz.Rect(word[0], word[1], word[2], word[3])
     pdf_page.draw_rect(rect, color=(1, 0, 0))
     text = word[4]
-    letters_top_0 = "ÄÖÜ"
-    letters_top_1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZbdfhikltj0123456789/\\§$%&?(){}[]"
-
-    letters_size_0 = "j(){}[]§"
-    letters_size_1 = "ÄÖÜ"
-    letters_size_2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZbdfhiklt0123456789/\\$%&?"
-    letters_size_3 = "gpqy"
-
-    font_size_pixel = 2
-
-    if bool(re.match(r'\w*[' + letters_top_0 + r']\w*', text)):
-        translation = 0.2
-    elif bool(re.match(r'\w*[' + letters_top_1 + r']\w*', text)):
-        translation = 0.45
-    else:
-        translation = 0.55
-
-    if bool(re.match(r'\w*[' + letters_size_1 + r']\w*', text)) and (
-            bool(re.match(r'\w*[' + letters_size_0 + r']\w*', text)) or bool(
-            re.match(r'\w*[' + letters_size_3 + r']\w*', text))):
-        scale = 0.9
-    elif bool(re.match(r'\w*[' + letters_size_2 + r']\w*', text)) and (
-            bool(re.match(r'\w*[' + letters_size_0 + r']\w*', text)) or bool(
-            re.match(r'\w*[' + letters_size_3 + r']\w*', text))):
-        scale = 1.0
-    elif bool(re.match(r'\w*[' + letters_size_3 + r']\w*', text)):
-        scale = 1.4
-
-    elif bool(re.match(r'\w*[' + letters_size_0 + r']\w*', text)):
-        scale = 1.1
-    elif bool(re.match(r'\w*[' + letters_size_1 + r']\w*', text)):
-        scale = 1.2
-    elif bool(re.match(r'\w*[' + letters_size_2 + r']\w*', text)):
-        scale = 1.40
-    else:
-        scale = 1.94
-
 
     scale_x = (rect.x1 - rect.x0) / fitz.get_text_length(text, fontsize=2)
-
+    #scale_x = (rect.x1 - rect.x0) / word[6]
+    scale_y = word[7]
     pdf_page.insert_textbox(rect, text, fontsize=2, color=(1, 0, 0), fontname='helv',
-                            morph=(fitz.Point(rect.x0, rect.y0 + font_size_pixel * translation),
-                                   fitz.Matrix(scale_x, scale * (rect.y1 - rect.y0) / font_size_pixel)))
+                            morph=(fitz.Point(rect.x0, rect.y0),
+                                   fitz.Matrix(scale_x,scale_y )))
 
     return pdf_page
 
@@ -105,9 +70,6 @@ def write_word_in_pdf_page(pdf_page:Page,word):
 def convert_pdf_to_multiple_jpeg(file_path: str, destination_folder: str):
     with Image(filename=file_path) as image:
         image.format = "jpg"
-        image.compression_quality = 100
-        image.resolution = 300
-        image.resize(210,297)
         filename = os.path.join(
             destination_folder, os.path.splitext(os.path.basename(file_path))[0] + ".jpg"
         )
