d0665460f6a30b9a0ff1415fa875a11c5e4a2c22
uegam <uegam@student.kit.edu>
2023-05-05 14:55:29 +0200

    Added Quickstart to README

--- src/ocrmypdf/test.py
+++ src/ocrmypdf/test.py
@@ -3,5 +3,6 @@ import ocrmypdf
 
 
 image = "/RIDSS2023/inputfolder/Testnotenauszug_scanned.pdf"
+image_jpg = "/RIDSS2023/inputfolder/Testnotenauszug-01.jpg"
 
-ocrmypdf.ocr(image,"test.pdf", deskew=True, image_dpi=300, skip_text=True)
+ocrmypdf.ocr(image_jpg,"outputfolder/pdf_with_ocr.pdf", deskew=True, image_dpi=300, skip_text=True)
